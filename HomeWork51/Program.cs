8a8c9a6f29f64001811db0823eac161f73ac5b7d
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-23 12:07:26 +0100

    Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы

---
+++ HomeWork51/Program.cs
@@ -0,0 +1,28 @@
+﻿// Найти точку пересечения двух прямых заданных 
+//уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+Console.WriteLine("Enter number k1: ");
+int k1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number b1: ");
+int b1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number k2: ");
+int k2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number b2: ");
+int b2 = int.Parse(Console.ReadLine());
+
+int y;
+int x;
+
+if (k1 != k2)
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * (b2 - b1) / (k1 - k2) + b1;
+    Console.WriteLine($"point: A ({x},{y})");
+}
+else
+{
+    Console.WriteLine($"parallel");
+}
