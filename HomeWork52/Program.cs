3d889107dee3fd76cd359ddaee460ca15a10dcc0
AlevtinaIudanova <iudanova87@bk.ru>
2023-02-23 12:09:29 +0100

    Написать программу копирования массива

---
+++ HomeWork52/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу копирования массива
+
+void Print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " | ");
+    }
+}
+
+int[] array = new int[6];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 15);
+}
+Print(array);
+Console.WriteLine();
+
+int[] copy = new int[array.Length];
+array.CopyTo(copy, 0);
+
+Console.WriteLine($"new array: {String.Join(", ", copy)}");
+
